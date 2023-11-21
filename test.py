f23114da76110e3bc4a9c8bf6b3b42040c73ee69
H <93993068+printincident@users.noreply.github.com>
2023-11-21 17:26:47 +0000

    Added Test Script
    Added Test script to get the schema from the postgres server

---
+++ test.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+
+DB_USER ="postgres"
+DB_PASS = "example"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME="postgres"
+try:
+	conn = psycopg2.connect(dbname=DB_NAME,
+							user=DB_USER,
+							password=DB_PASS,
+							host=DB_HOST,
+							port=DB_PORT)
+	print("Database connected successfully")
+except:
+    print("Database not connected successfully")
+    exit()
+
+query:str = """
+        SELECT table_name, column_name, data_type
+        FROM information_schema.columns
+        WHERE table_schema = 'public'  -- Change 'public' to your schema name if needed
+        ORDER BY table_name, ordinal_position
+        """
+with conn.cursor() as curs:
+	try:
+		curs.execute(query)
+		schema_info = curs.fetchall()
+		for table_name, column_name, data_type in schema_info:
+			print(f"Table: {table_name}, Column: {column_name}, Type: {data_type}")
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+		
+
+
+
+conn.close()
