8eb011bc4a519799b83c4c0f7ebc53e24b55b893
Matthew Turner <turnermatthew7@gmail.com>
2023-12-04 18:20:04 +0000

    feature: created class to generate plotly graphs from sensordata

---
+++ web_ui/plotly_handler.py
@@ -0,0 +1,54 @@
+# imports
+
+
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as pgo
+
+
+class PlotlyHandler:
+
+    def __init__(self):
+
+        pass
+
+    def gen_sensordata_line_chart(self, sensordata_df: pd.DataFrame, data_limits_df: pd.DataFrame) -> pgo.Figure:
+
+        output_graph = pgo.Figure()
+
+        output_graph.add_trace(
+            pgo.Scatter(
+                x=sensordata_df["date_recorded"],
+                y=sensordata_df["temperature"],
+                mode="lines+markers",
+                name="sensordata"
+            )
+        )
+
+        output_graph.add_shape(
+            type="line",
+            x0=sensordata_df["date_recorded"].min(), y0=data_limits_df["temperature_min"][0],
+            x1=sensordata_df["date_recorded"].max(), y1=data_limits_df["temperature_min"][0],
+            line=dict(color="red", width=3),
+            name="temp"
+        )
+
+        output_graph.add_shape(
+            type="line",
+            x0=sensordata_df["date_recorded"].min(), y0=data_limits_df["temperature_max"][0],
+            x1=sensordata_df["date_recorded"].max(), y1=data_limits_df["temperature_max"][0],
+            line=dict(color="red", width=3),
+            name="temp"
+        )
+
+        output_graph.update_layout(
+            title="Sensordata Over Time",
+            xaxis_title="Date Recorded",
+            yaxis_title="Data Values",
+            legend_title="Legend",
+            template="plotly_dark"
+        )
+
+        output_graph.layout.autosize = True
+
+        return output_graph
